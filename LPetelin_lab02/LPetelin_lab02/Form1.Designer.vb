6c6f6a084a95848376efe0ada1d463740f702165
lpetelin0427 <lukepetelin@gmail.com>
2020-06-03 12:47:11 -0400

    Add files via upload

---
+++ LPetelin_lab02/LPetelin_lab02/Form1.Designer.vb
@@ -0,0 +1,302 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim TreeNode127 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem01")
+        Dim TreeNode128 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item11", New System.Windows.Forms.TreeNode() {TreeNode127})
+        Dim TreeNode129 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem2")
+        Dim TreeNode130 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item12", New System.Windows.Forms.TreeNode() {TreeNode129})
+        Dim TreeNode131 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem3")
+        Dim TreeNode132 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item13", New System.Windows.Forms.TreeNode() {TreeNode131})
+        Dim TreeNode133 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem4")
+        Dim TreeNode134 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item14", New System.Windows.Forms.TreeNode() {TreeNode133})
+        Dim TreeNode135 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem5")
+        Dim TreeNode136 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item15", New System.Windows.Forms.TreeNode() {TreeNode135})
+        Dim TreeNode137 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem6")
+        Dim TreeNode138 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item17", New System.Windows.Forms.TreeNode() {TreeNode137})
+        Dim TreeNode139 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem7")
+        Dim TreeNode140 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item18", New System.Windows.Forms.TreeNode() {TreeNode139})
+        Dim TreeNode141 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem8")
+        Dim TreeNode142 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item19", New System.Windows.Forms.TreeNode() {TreeNode141})
+        Dim TreeNode143 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("SubItem9")
+        Dim TreeNode144 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Item20", New System.Windows.Forms.TreeNode() {TreeNode143})
+        Dim ListViewItem5 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("", 0)
+        Dim ListViewItem6 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("", 1)
+        Dim ListViewItem7 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("", 2)
+        Dim ListViewItem8 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("", 3)
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ImageList2 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {"Item01", "Item02", "Item03", "Item04", "Item05", "Item06", "Item07", "Item08", "Item09", "Item10"})
+        Me.ListBox1.Location = New System.Drawing.Point(250, 72)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(157, 260)
+        Me.ListBox1.TabIndex = 0
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(561, 72)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode127.Name = "SubItem01"
+        TreeNode127.Text = "SubItem01"
+        TreeNode128.Name = "Item11"
+        TreeNode128.Text = "Item11"
+        TreeNode129.Name = "SubItem2"
+        TreeNode129.Text = "SubItem2"
+        TreeNode130.Name = "Item12"
+        TreeNode130.Text = "Item12"
+        TreeNode131.Name = "SubItem3"
+        TreeNode131.Text = "SubItem3"
+        TreeNode132.Name = "Item13"
+        TreeNode132.Text = "Item13"
+        TreeNode133.Name = "SubItem4"
+        TreeNode133.Text = "SubItem4"
+        TreeNode134.Name = "Item14"
+        TreeNode134.Text = "Item14"
+        TreeNode135.Name = "SubItem5"
+        TreeNode135.Text = "SubItem5"
+        TreeNode136.Name = "Item15"
+        TreeNode136.Text = "Item15"
+        TreeNode137.Name = "SubItem6"
+        TreeNode137.Text = "SubItem6"
+        TreeNode138.Name = "Item16"
+        TreeNode138.Text = "Item17"
+        TreeNode139.Name = "SubItem7"
+        TreeNode139.Text = "SubItem7"
+        TreeNode140.Name = "Item18"
+        TreeNode140.Text = "Item18"
+        TreeNode141.Name = "SubItem8"
+        TreeNode141.Text = "SubItem8"
+        TreeNode142.Name = "Item19"
+        TreeNode142.Text = "Item19"
+        TreeNode143.Name = "SubItem9"
+        TreeNode143.Text = "SubItem9"
+        TreeNode144.Name = "Item20"
+        TreeNode144.Text = "Item20"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode128, TreeNode130, TreeNode132, TreeNode134, TreeNode136, TreeNode138, TreeNode140, TreeNode142, TreeNode144})
+        Me.TreeView1.Size = New System.Drawing.Size(163, 254)
+        Me.TreeView1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(275, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "List Box"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(594, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "TreeView"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(444, 93)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = ">"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(444, 157)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "<"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(38, 112)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(190, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Add Item to ListBox"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(38, 157)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(190, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Delete Item from ListBox"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(38, 72)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(190, 22)
+        Me.TextBox1.TabIndex = 8
+        '
+        'ListView1
+        '
+        Me.ListView1.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem5, ListViewItem6, ListViewItem7, ListViewItem8})
+        Me.ListView1.LargeImageList = Me.ImageList1
+        Me.ListView1.Location = New System.Drawing.Point(22, 275)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(206, 117)
+        Me.ListView1.TabIndex = 9
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "blueIcon.png")
+        Me.ImageList1.Images.SetKeyName(1, "greenICon.png")
+        Me.ImageList1.Images.SetKeyName(2, "Hanzo_Spray_-_Icon.png")
+        Me.ImageList1.Images.SetKeyName(3, "lucioicon.jpg")
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(86, 255)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 17)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "ListView"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(730, 70)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(146, 23)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Expand TreeView"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(730, 111)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(146, 23)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "Collapse TreeView"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ImageList2
+        '
+        Me.ImageList2.ImageStream = CType(resources.GetObject("ImageList2.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList2.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList2.Images.SetKeyName(0, "babywinston.jpg")
+        Me.ImageList2.Images.SetKeyName(1, "lucio.png")
+        Me.ImageList2.Images.SetKeyName(2, "macOS-Sierra-Wallpaper-Macbook-Wallpaper.jpg")
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(894, 70)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(231, 254)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 13
+        Me.PictureBox1.TabStop = False
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(894, 336)
+        Me.TrackBar1.Maximum = 3
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(231, 56)
+        Me.TrackBar1.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1268, 447)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Label3 As Label
+    Private WithEvents TreeView1 As TreeView
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents ImageList2 As ImageList
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TrackBar1 As TrackBar
+End Class
---
+++ LPetelin_lab02/LPetelin_lab02/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private imageNum As Integer = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '*********Couldn't figure out how to add multiple items to the TreeView so I just left it at one. Spent 2 hours googling************
+
+        If ListBox1.SelectedItem IsNot Nothing Then
+            TreeView1.Nodes.Add(New TreeNode(ListBox1.SelectedItem))
+        Else
+            MessageBox.Show("Please select an Item from the ListBox to add to the TreeView")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TreeView1.SelectedNode IsNot Nothing Then
+            ListBox1.Items.Add(TreeView1.SelectedNode)
+        Else
+            MessageBox.Show("Please select a Node from the Treeview to add to the ListBox")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text IsNot "" Then
+            ListBox1.Items.Add(TextBox1.Text)
+        Else
+            MessageBox.Show("Please enter an item to add to the ListBox")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TreeView1.ExpandAll()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TreeView1.CollapseAll()
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+
+        Static counter As Integer = 1
+
+        If counter > ImageList2.Images.Count - 1 Then
+            counter = 0
+        End If
+        PictureBox1.Image = ImageList2.Images(counter)
+        counter += 1
+    End Sub
+End Class

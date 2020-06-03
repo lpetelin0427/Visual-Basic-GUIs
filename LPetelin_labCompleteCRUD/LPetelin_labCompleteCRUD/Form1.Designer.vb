7dfa00fd7cb665888183789b4127b71bebdd6eb5
lpetelin0427 <lukepetelin@gmail.com>
2020-06-03 12:59:02 -0400

    Add files via upload

---
+++ LPetelin_labCompleteCRUD/LPetelin_labCompleteCRUD/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtFname = New System.Windows.Forms.TextBox()
+        Me.txtLname = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.txtEditFname = New System.Windows.Forms.TextBox()
+        Me.txtEditLname = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(334, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Here is where you can insert items to the Database:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "First Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 109)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Last Name"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(96, 42)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(100, 22)
+        Me.txtID.TabIndex = 4
+        '
+        'txtFname
+        '
+        Me.txtFname.Location = New System.Drawing.Point(96, 75)
+        Me.txtFname.Name = "txtFname"
+        Me.txtFname.Size = New System.Drawing.Size(100, 22)
+        Me.txtFname.TabIndex = 5
+        '
+        'txtLname
+        '
+        Me.txtLname.Location = New System.Drawing.Point(96, 109)
+        Me.txtLname.Name = "txtLname"
+        Me.txtLname.Size = New System.Drawing.Size(100, 22)
+        Me.txtLname.TabIndex = 6
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(68, 149)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 7
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(495, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(296, 34)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Here is where you can view, update, or delete" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " the items in the HW Database:"
+        '
+        'btnView
+        '
+        Me.btnView.Location = New System.Drawing.Point(498, 46)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(75, 23)
+        Me.btnView.TabIndex = 9
+        Me.btnView.Text = "View"
+        Me.btnView.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(477, 123)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(446, 172)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(592, 46)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 11
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(689, 46)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 12
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'txtEditFname
+        '
+        Me.txtEditFname.Location = New System.Drawing.Point(557, 88)
+        Me.txtEditFname.Name = "txtEditFname"
+        Me.txtEditFname.Size = New System.Drawing.Size(100, 22)
+        Me.txtEditFname.TabIndex = 13
+        '
+        'txtEditLname
+        '
+        Me.txtEditLname.Location = New System.Drawing.Point(772, 88)
+        Me.txtEditLname.Name = "txtEditLname"
+        Me.txtEditLname.Size = New System.Drawing.Size(100, 22)
+        Me.txtEditLname.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(474, 91)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(80, 17)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "First Name:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(686, 91)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(80, 17)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Last Name:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(935, 322)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtEditLname)
+        Me.Controls.Add(Me.txtEditFname)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnView)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtLname)
+        Me.Controls.Add(Me.txtFname)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtFname As TextBox
+    Friend WithEvents txtLname As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnView As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents txtEditFname As TextBox
+    Friend WithEvents txtEditLname As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ LPetelin_labCompleteCRUD/LPetelin_labCompleteCRUD/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    Public index As Integer = 0
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Using db As New Database1Entities
+            Dim p = New Table()
+            p.FirstName = txtFname.Text
+            p.LastName = txtLname.Text
+            p.Id = CInt(txtID.Text)
+            db.Tables.Add(p)
+            db.SaveChanges()
+            btnView.PerformClick()
+        End Using
+    End Sub
+
+    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
+        Using db As New Database1Entities
+            Dim query = From p In db.Tables
+                        Select p.Id, p.FirstName, p.LastName
+
+            DataGridView1.DataSource = query.ToList()
+
+        End Using
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        index = DataGridView1.SelectedRows(0).Cells(0).Value.ToString()
+        txtEditFname.Text = DataGridView1.SelectedRows(0).Cells(1).Value.ToString
+        txtEditLname.Text = DataGridView1.SelectedRows(0).Cells(2).Value.ToString
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Try
+            Using db As New Database1Entities
+                Dim toDelete = db.Tables.Where(Function(o) o.Id = index).FirstOrDefault()
+                If Not IsNothing(toDelete) Then
+                    db.Tables.Remove(toDelete)
+                End If
+                db.SaveChanges()
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("hey error fam" & ex.Message)
+        End Try
+        btnView.PerformClick()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            Using db As New Database1Entities
+                Dim toEdit = (From p In db.Tables
+                              Where p.Id = index
+                              Select p).FirstOrDefault()
+                toEdit.FirstName = txtEditFname.Text
+                toEdit.LastName = txtEditLname.Text
+
+                db.SaveChanges()
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("hey error fam" & ex.Message)
+        End Try
+        btnView.PerformClick()
+    End Sub
+End Class
